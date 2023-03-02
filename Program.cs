8740ac6d61f970ba3e26373bc78bdd7cf4a958d5
lcsmota <sacul_m@protonmail.com>
2023-03-02 09:12:06 -0300

    Feat: register services in DI Container

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
+using System.Reflection;
+using MediatR;
+using StudentCQRS.Data;
+using StudentCQRS.Repositories;
+using StudentCQRS.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+    builder.Services.AddDbContext<StudentDbContext>();
+    builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
     builder.Services.AddControllers();
 
     builder.Services.AddEndpointsApiExplorer();
